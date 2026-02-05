dd45045e9b4cf9e9b8e12300a8afa5f53a12fc0a
Dushyant Gaur <dushyant.gaur_cs25@gla.ac.in>
2026-02-05 10:01:18 +0530

    fix mongo atlas connection

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/project1")
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
